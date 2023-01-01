7591a229d65263cf120fc389b8965a8358764913
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2023-01-01 21:17:08 +0100

    add openapi

---
+++ Source/CSharpAmazonBusinessAPI/Exceptions/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpAmazonBusinessAPI.Exceptions
+{
+    public class ApiException<T> : Exception
+    {
+        public ApiException() { }
+        public ApiException(string message, int status_, string text, System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>> headers_, object p, object p1) { }
+
+        //public ApiException(string message, Exception inner)
+        //: base(message, inner) { }
+    }
+}
